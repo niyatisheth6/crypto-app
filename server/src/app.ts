0600f4c6bbdc1a11ef1c81108e3eb73798375c57
Niyati sheth <niyati.radicalloop@gmail.com>
2025-08-01 18:04:37 +0530

    fix : fix cors error

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 app.use(
-  cors({ origin: [process.env.FRONTEND_URL as string], credentials: true })
+  cors({ origin: process.env.FRONTEND_URL as string, credentials: true })
 );
 
 app.use(express.json());
